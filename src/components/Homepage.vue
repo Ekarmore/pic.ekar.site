0b63004135ef177ff8ec2a0e17f15427576995a7
EKar <Earthb0y@outlook.com>
2022-08-28 12:47:48 +0800

    update

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
   <section>
-   <div class="title"><p>Ekar</p></div> 
+   <div class="title"><p>Ekar</p><p class="title-two">Photographer</p></div> 
     <div class="catalog">
     <div class="img-a"><router-link class="link" to="page">Unnoticed</router-link><img class="img-card" src="../assets/01.png" alt=""></div>
     <div class="img-a"><router-link class="link" to="page">a</router-link><img class="img-card" src="../assets/01.png" alt=""></div>
@@ -18,18 +18,21 @@ export default {};
 
 <style>
 .catalog{ 
-@apply w-11/12  md:w-3/5 mx-auto flex flex-wrap justify-center mt-20
+@apply mt-10 w-11/12 mx-auto flex flex-wrap justify-center md:w-3/5 md:mt-10 
 }
 .title{
-@apply md:w-3/5  mt-5 mx-auto text-lg font-serif 
+@apply w-11/12 md:w-2/5  mt-10 mx-auto text-xl font-serif 
+}
+.title-two{
+@apply text-base opacity-80
 }
 .img-card{
-@apply mb-5 md:w-72 md:m-1
+@apply mb-5 md:w-72 md:m-1 bg-green-200
 }
 .img-a{
 @apply relative
 }
 .link{
-@apply absolute text-white w-full h-full text-center text-3xl font-thin font-serif flex justify-center items-center opacity-0 hover:opacity-100 transition-all ease-in-out 
+@apply absolute text-white w-full h-full text-center text-3xl font-thin font-serif flex justify-center items-center  md:opacity-0 md:hover:opacity-100 transition-all ease-in-out 
 }
 </style>
