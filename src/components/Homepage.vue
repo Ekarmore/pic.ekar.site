82a896b053af413753eeddf4fa5db227c4eb0fb5
EKar <Earthb0y@outlook.com>
2022-08-28 15:51:49 +0800

    add

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,7 +3,7 @@
   <div>
    <div class="title"><router-link to="/">Ekar</router-link><p class="title-two">Photographer</p></div> 
     <div class="catalog">
-    <div class="img-a"><router-link class="link" to="page">Unnoticed</router-link><img class="img-card" src="../assets/Turpan.JPG" alt=""></div>
+    <div class="img-a"><router-link class="link" to="page">Unnoticed</router-link><img class="img-card" src="../assets/01.png" alt=""></div>
     <div class="img-a"><router-link class="link" to="page">a</router-link><img class="img-card" src="../assets/01.png" alt=""></div>
     <div class="img-a"><a class="link" href="">a</a><img class="img-card" src="../assets/01.png" alt=""></div>
     <div class="img-a"><a class="link" href="">Coming Soon</a><img class="img-card" src="../assets/01.png" alt=""></div>
