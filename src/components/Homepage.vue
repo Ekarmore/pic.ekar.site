aeaa3f1c300accf55d599a76e6be51ec955da759
EKar <Earthb0y@outlook.com>
2022-08-29 23:28:27 +0800

    a

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,19 +1,33 @@
 <template>
 
   <div>
-  <section>
-    <header class="header"><router-link to="Work">Ekar</router-link><p class="title-two">Photographer</p></header> 
-    <div class="catalog">
-    <div class="img-a"><router-link class="link" to="Unnoticed">Unnoticed</router-link><img class="img-card" src="../assets/002.jpg" alt=""></div>
-    <div class="img-a"><router-link class="link" to="page">a</router-link><img class="img-card" src="../assets/002.jpg" alt=""></div>
-    </div>
-    <div class="catalog-2">
-    <div class="img-a"><router-link class="link" to="page">Unnoticed</router-link><img class="img-card" src="../assets/002.jpg" alt=""></div>
-    <div class="img-a"><router-link class="link" to="page">a</router-link><img class="img-card" src="../assets/002.jpg" alt=""></div>
-    </div>
-    <footer class="footer">Design and create by <a class="footer-link" href="https://ekar.site">Ekar</a> in 2022</footer>
-  </section>
-   </div>
+    <section>
+        <header class="header">
+        <router-link to="Work">Ekar</router-link>
+        <p class="title-two">Photographer</p>
+      </header>
+      <div class="catalog">
+        <div class="img-a">
+          <router-link class="link" to="Unnoticed">Unnoticed</router-link><img class="img-card" src="../assets/002.jpg"
+            alt="">
+        </div>
+        <div class="img-a">
+          <router-link class="link" to="page">a</router-link><img class="img-card" src="../assets/002.jpg" alt="">
+        </div>
+      </div>
+      <div class="catalog-2">
+        <div class="img-a">
+          <router-link class="link" to="page">Unnoticed</router-link><img class="img-card" src="../assets/002.jpg"
+            alt="">
+        </div>
+        <div class="img-a">
+          <router-link class="link" to="page">a</router-link><img class="img-card" src="../assets/002.jpg" alt="">
+        </div>
+      </div>
+      <footer class="footer">Design and create by <a class="footer-link" href="https://ekar.site">Ekar</a> in 2022
+      </footer>
+    </section>
+  </div>
 </template> 
 
 <script>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,7 +4,7 @@
 
 <template>
 <nav class="nav-bar">
-<span class="nav-text"> <router-link to="Unnoticed">Unnoticed</router-link></span>
+<span class="nav-text"> <router-link active-class="active" to="Unnoticed">Unnoticed</router-link></span>
 <span class="nav-text">Unnoticed</span>
 <span class="nav-text">Unnoticed</span>
 <span class="nav-text">Unnoticed</span>
@@ -13,9 +13,12 @@
 
 <style scoped>
 .nav-bar{
-@apply md:w-1/2 mx-auto text-center m-10 
+@apply xl:w-2/5 mx-auto text-center m-10
 }
 .nav-text{
-@apply text-lg m-2 font-light font-serif
+@apply text-base font-serif m-5 font-light text-gray-400 hover:text-black transition-all ease-in-out duration-500
+}
+.active{
+@apply text-black
 }
 </style>
