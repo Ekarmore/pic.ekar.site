80623d4c02241beb15e745a4af9511f0f7b33005
EKar <Earthb0y@outlook.com>
2022-08-29 16:26:32 +0800

    a

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,14 +3,19 @@
 </script>
 
 <template>
-<nav class="text-xl text-center font-light mt-10">
-<span class="p-5"> <router-link to="Unnoticed">Unnoticed</router-link></span>
-<span class="p-5">Unnoticed</span>
-<span class="p-5">Unnoticed</span>
-<span class="p-5">Unnoticed</span>
+<nav class="nav-bar">
+<span class="nav-text"> <router-link to="Unnoticed">Unnoticed</router-link></span>
+<span class="nav-text">Unnoticed</span>
+<span class="nav-text">Unnoticed</span>
+<span class="nav-text">Unnoticed</span>
 </nav>
 </template>
 
 <style scoped>
-
+.nav-bar{
+@apply md:w-1/2 mx-auto text-center m-10 
+}
+.nav-text{
+@apply text-lg m-2 font-light font-serif
+}
 </style>
--- src/components/Unnoticed.vue
+++ src/components/Unnoticed.vue
@@ -35,8 +35,9 @@ export default {
                     说说我最近常去的江边，起初大概是因为离我的居所很近加之所在的大学城岛也并不大，如果想要出门走走会很快就走到江边。但以前大多都会骑车路过，所以会没有留意到许多。很多次路过环岛的几处江边觉得可能会有自己想要走近看看的风景可也没有真正的停下来去看，之后因为空闲时间变多和去江边实在是容易，会常常在江边徒步。在江边行走几次之后我会感到有些奇怪，江水将岛分割了起来，这让江边成为了岛上不只是地理上的最边缘，被扔在草坪上生产日期是前几个月的食品包装袋或是被植缠满的生锈单车都在说明着这里鲜有被打理的时候。好在它并不就此死寂，当走在江边会看到许多景象，它们由充满生机的部分构成。
                 </p>
             </div>
-            <div class="pic-container">
-                    <img class="pic" src="../assets/002.jpg" alt="">
+            <section class="pic-box">
+                <div class="pic-container">
+                <img class="pic" src="../assets/002.jpg" alt="">
             </div>
             <div class="wow fadeIn" data-wow-duration="1s">
                 <div class="pic-container">
@@ -58,6 +59,7 @@ export default {
                     <img class="pic" src="../assets/002.jpg" alt="">
                 </div>
             </div>
+            </section>
         </div>
         <footer class="footer-work">
             <div class="wow fadeInLeft" data-wow-duration="1s">
@@ -68,31 +70,5 @@ export default {
 </template>
 
 <style >
-.Work-container {
-    @apply w-full h-full mt-10
-}
 
-.title-container {
-    @apply w-11/12 xl:w-2/5 mx-auto leading-7 italic
-}
-
-.title-text {
-    @apply text-2xl italic
-}
-
-.title-text2 {
-    @apply text-2xl mb-5 italic
-}
-
-.pic-container {
-    @apply w-11/12 mx-auto mt-5
-}
-
-.pic {
-    @apply mx-auto
-}
-
-.footer-work {
-    @apply mt-10
-}
 </style>
