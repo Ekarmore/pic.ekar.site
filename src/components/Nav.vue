a43abbe1e0bc15a0d4ca2f1f6fba7b114e6e985f
EKar <Earthb0y@outlook.com>
2022-08-29 23:50:11 +0800

    a

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -13,7 +13,7 @@
 
 <style scoped>
 .nav-bar{
-@apply w-2/5 md:w-4/5 xl:w-2/5 mx-auto text-center m-10 flex flex-wrap justify-center 
+@apply w-2/5  md:w-4/5 xl:w-2/5 mx-auto text-center m-10 flex flex-wrap justify-center 
 }
 .nav-text{
 @apply text-xl mb-2 md:m-5  md:text-base font-serif  font-light text-gray-400 hover:text-black transition-all ease-in-out duration-500
