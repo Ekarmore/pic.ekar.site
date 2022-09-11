1bb527dbdd9f49f95f92701a2664eacc1b6b3c2b
EKar <Earthb0y@outlook.com>
2022-09-11 11:38:29 +0800

    change

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -4,7 +4,7 @@
 
 <template>
 <nav class="nav-bar">
-<span class="nav-text"> <router-link active-class="active" to="/">Home</router-link></span>
+<span class="nav-text"> <router-link active-class="active" to="/">Catalog</router-link></span>
 <span class="nav-text"> <router-link active-class="active" to="Unnoticed">Unnoticed</router-link></span>
 <span class="nav-text"> <router-link active-class="active" to="/">portrait</router-link></span>
 <span class="nav-text"> <router-link active-class="active" to="/">Coming Soon</router-link></span>
@@ -13,7 +13,7 @@
 
 <style >    
 .nav-bar{
-@apply  w-4/5  md:w-4/5 xl:w-2/5 mx-auto text-center m-10 flex flex-col justify-center lg:flex-row
+@apply  w-4/5  md:w-4/5 xl:w-2/5 mx-auto text-center m-10 flex flex-col justify-center lg:flex-row 
 }
 .nav-text{
 @apply text-xl m-2 font-serif  font-light text-gray-400 hover:text-black transition-all ease-in-out duration-500
