dd395fec62568f0b3448f876a14ab6dfe7324c19
EKar <Earthb0y@outlook.com>
2022-09-18 18:45:13 +0800

    change

--- src/components/Another_Landscape.vue
+++ src/components/Another_Landscape.vue
@@ -29,14 +29,18 @@ export default {
     <div>
         <main class="Work-container">
             <div class="title-container">
+                <div class="wow fadeInLeft" data-wow-duration="2s">
                 <h1 class="title-text"><span>
                     </span> Another Landscape</h1>
-                    <div class="wow fadeInLeft" data-wow-duration="1s">
+                </div>
+                    <div class="wow fadeInLeft" data-wow-duration="2s">
                     <p class="text-lg mx-auto pb-40 pt-40 text-center border-2  border-black">
                    Coming Soon
                 </p>
                 </div>
+                <div class="wow fadeInRight" data-wow-duration="2s">
                 <h1 class="title-text3">2021-Now</h1>
+                </div>
             </div>
             <!-- <section class="pic-box">
                 <div class="pic-container">
