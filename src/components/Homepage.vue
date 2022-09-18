4e886c8fffc0d64a1df1ab7002b8deec926b7445
EKar <Earthb0y@outlook.com>
2022-09-18 17:29:06 +0800

    change

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <main>
+    `<main>
       <header class="header">
         <router-link to="Work">Ekar</router-link>
         <p class="title-two">Photographer</p>
@@ -11,21 +11,21 @@
             alt="">
         </div>
         <div class="img-a">
-          <router-link class="link" to="page">a</router-link><img class="img-card" src="../assets/05.jpg" alt="">
+          <router-link class="link" to="page">Coming Soon</router-link><img class="img-card" src="../assets/Zw.jpg" alt="">
         </div>
       </section>
       <div class="catalog-2">
         <div class="img-a">
-          <router-link class="link" to="page">Unnoticed</router-link><img class="img-card" src="../assets/002.jpg"
+          <router-link class="link" to="page">Coming Soon</router-link><img class="img-card" src="../assets/Zw.jpg"
             alt="">
         </div>
         <div class="img-a">
-          <router-link class="link" to="page">a</router-link><img class="img-card" src="../assets/002.jpg" alt="">
+          <router-link class="link" to="page">Coming Soon</router-link><img class="img-card" src="../assets/Zw.jpg" alt="">
         </div>
       </div>
       <footer class="footer">Design and create by <a class="footer-link" href="https://ekar.site">Ekar</a> in 2022
       </footer>
-    </main>
+    </main>`
   </div>
 </template> 
 
