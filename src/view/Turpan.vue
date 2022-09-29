bad810f03a52a22288ff3f048622a97841226b46
EKar <Earthb0y@outlook.com>
2022-09-29 17:35:28 +0800

    col box

--- src/view/Turpan.vue
+++ src/view/Turpan.vue
@@ -6,142 +6,74 @@ import { onMounted, ref } from "vue";
 const [a, a1, a2, a3] = [ref(false), ref(false), ref(false), ref(false)];
 
 const [imgLoad, imgLoad2, imgLoad3, imgLoad4] = [
-  () => {
-    a.value = true;
-  },
-  () => {
-    a1.value = true;
-  },
-  () => {
-    a2.value = true;
-  },
-  () => {
-    a3.value = true;
-  },
+    () => {
+        a.value = true;
+    },
+    () => {
+        a1.value = true;
+    },
+    () => {
+        a2.value = true;
+    },
+    () => {
+        a3.value = true;
+    },
 ];
-const xbox = ref(null);
+const colBox = ref(null);
+
+//wow init
 
 new WOW({
-  boxClass: "wow",
-  animateClass: "animated",
-  offset: 0,
-  mobile: true,
-  live: true,
-  callback: function (box) {
-    // the callback is fired every time an animation is started
-    // the argument that is passed in is the DOM node being animated
-  },
-  scrollContainer: null,
-  resetAnimation: true, // reset animation on end (default is true)
+    boxClass: "wow",
+    animateClass: "animated",
+    offset: 0,
+    mobile: true,
+    live: true,
+    callback: function (box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,
+    resetAnimation: true, // reset animation on end (default is true)
 }).init();
 
 const wheel = (event) => {
-//   event.preventDefault();
-  xbox.value.scrollLeft += event.deltaY;
+    //   event.preventDefault();
+    colBox.value.scrollLeft += event.deltaY;
 };
 </script>
 <template>
-  <section v-show="false">
-    <main class="Work-container">
-      <div class="title-container">
-        <h1 class="title-text">
-          <span>
-            <router-link to="/"></router-link>
-          </span>
-          Unnoticed
-        </h1>
-        <p class="about-text">
-          说说我最近常去的江边，起初大概是因为离我的居所很近加之所在的大学城岛也并不大，如果想要出门走走会很快就走到江边。但以前大多都会骑车路过，所以会没有留意到许多。很多次路过环岛的几处江边觉得可能会有自己想要走近看看的风景可也没有真正的停下来去看，之后因为空闲时间变多和去江边实在是容易，会常常在江边徒步。在江边行走几次之后我会感到有些奇怪，江水将岛分割了起来，这让江边成为了岛上不只是地理上的最边缘，被扔在草坪上生产日期是前几个月的食品包装袋或是被植缠满的生锈单车都在说明着这里鲜有被打理的时候。好在它并不就此死寂，当走在江边会看到许多景象，它们由充满生机的部分构成。
-        </p>
-        <h1 class="title-text3">2021/5</h1>
-      </div>
-      <section class="pic-box">
-        <div class="pic-container">
-          <img
-            @load="imgLoad"
-            class="pic"
-            src="../assets/Turpan_020.jpg"
-            alt=""
-          />
-        </div>
-        <div class="wow fadeIn" data-wow-duration="1s">
-          <div class="pic-container">
-            <img
-              @load="imgLoad2"
-              class="pic"
-              src="../assets/Turpan_01.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div class="wow fadeIn" data-wow-duration="1s">
-          <div class="pic-container">
-            <img
-              @load="imgLoad3"
-              class="pic"
-              src="../assets/Turpan_03.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-        <div class="wow fadeIn" data-wow-duration="1s">
-          <div class="pic-container">
-            <img @load="imgLoad4" class="pic" src="../assets/B10.jpg" alt="" />
-          </div>
-        </div>
-        <div class="wow fadeIn" data-wow-duration="1s">
-          <div class="pic-container">
-            <img class="pic" src="../assets/B5.jpg" alt="" />
-          </div>
-        </div>
-        <div class="wow fadeIn" data-wow-duration="1s">
-          <div class="pic-container">
-            <img class="pic" src="../assets/B6.jpg" alt="" />
-          </div>
-        </div>
-        <div class="wow fadeIn" data-wow-duration="1s">
-          <div class="pic-container">
-            <img class="pic" src="../assets/B7.jpg" alt="" />
-          </div>
-        </div>
-      </section>
-    </main>
-    <footer class="footer-work">
-      <div class="wow fadeInLeft" data-wow-duration="2s">
-        <Nav />
-      </div>
-    </footer>
-  </section>
-
-  <section class="menu">
-    <Nav class="Nav-bar" />
-  </section>
+    <section class="navBar">
+        <Nav class="Nav-bar" />
+    </section>
     <section class="main-box">
-    <div ref="xbox" class="xbox2" @wheel="wheel">
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
+        <div ref="colBox" class="col-box" @wheel="wheel">
+            <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
 
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_02.jpg" alt="" />
+            <img height="600px" @load="imgLoad" class="pic" src="../assets/Turpan_02.jpg" alt="" />
 
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_01.jpg" alt="" />
+            <img @load="imgLoad" class="pic" src="../assets/Turpan_01.jpg" alt="" />
 
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
+            <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
 
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
+            <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
 
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
+            <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
 
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
+            <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
 
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
-    </div>
-  </section>
+            <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
+        </div>
+    </section>
 
-  <section class="hp-loading" v-show="false">
-    <!-- <p class="loading-text">Ekar</p> -->
-    <div class="loading">
-      <div></div>
-    </div>
-  </section>
+    <section class="hp-loading" v-show="false">
+        <!-- <p class="loading-text">Ekar</p> -->
+        <div class="loading">
+            <div></div>
+        </div>
+    </section>
 </template>
 
-<style></style>
+<style>
+
+</style>
