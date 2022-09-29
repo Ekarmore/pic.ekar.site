e1004def2c7a88638021ac8ed1da56ca011b0236
EKar <Earthb0y@outlook.com>
2022-09-29 17:49:12 +0800

    col box

--- src/view/Turpan.vue
+++ src/view/Turpan.vue
@@ -6,74 +6,76 @@ import { onMounted, ref } from "vue";
 const [a, a1, a2, a3] = [ref(false), ref(false), ref(false), ref(false)];
 
 const [imgLoad, imgLoad2, imgLoad3, imgLoad4] = [
-    () => {
-        a.value = true;
-    },
-    () => {
-        a1.value = true;
-    },
-    () => {
-        a2.value = true;
-    },
-    () => {
-        a3.value = true;
-    },
+  () => {
+    a.value = true;
+  },
+  () => {
+    a1.value = true;
+  },
+  () => {
+    a2.value = true;
+  },
+  () => {
+    a3.value = true;
+  },
 ];
+
+// 获取 colBox
 const colBox = ref(null);
 
 //wow init
-
 new WOW({
-    boxClass: "wow",
-    animateClass: "animated",
-    offset: 0,
-    mobile: true,
-    live: true,
-    callback: function (box) {
-        // the callback is fired every time an animation is started
-        // the argument that is passed in is the DOM node being animated
-    },
-    scrollContainer: null,
-    resetAnimation: true, // reset animation on end (default is true)
+  boxClass: "wow",
+  animateClass: "animated",
+  offset: 0,
+  mobile: true,
+  live: true,
+  callback: function (box) {
+    // the callback is fired every time an animation is started
+    // the argument that is passed in is the DOM node being animated
+  },
+  scrollContainer: null,
+  resetAnimation: true, // reset animation on end (default is true)
 }).init();
 
+// 滚轮事件
 const wheel = (event) => {
-    //   event.preventDefault();
-    colBox.value.scrollLeft += event.deltaY;
+  //阻止默认事件触发
+  //   event.preventDefault();
+  // 滚动
+  colBox.value.scrollLeft += event.deltaY;
 };
 </script>
 <template>
-    <section class="navBar">
-        <Nav class="Nav-bar" />
-    </section>
-    <section class="picture_container">
-        <div ref="colBox" class="col-box" @wheel="wheel">
-            <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
+  <section class="navBar">
+    <Nav class="Nav-bar" />
+  </section>
+  <section class="picture_container">
+    <div ref="colBox" class="col-box" @wheel="wheel">
+      <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
 
-            <img @load="imgLoad" class="pic" src="../assets/Turpan_02.jpg" alt="" />
+      <img @load="imgLoad" class="pic" src="../assets/Turpan_02.jpg" alt="" />
 
-            <img @load="imgLoad" class="pic" src="../assets/Turpan_01.jpg" alt="" />
+      <img @load="imgLoad" class="pic" src="../assets/Turpan_01.jpg" alt="" />
 
-            <img @load="imgLoad" class="pic" src="../assets/Turpan_03.jpg" alt="" />
+      <img @load="imgLoad" class="pic" src="../assets/Turpan_03.jpg" alt="" />
 
-            <img @load="imgLoad" class="pic" src="../assets/Turpan_04.jpg" alt="" />
+      <img @load="imgLoad" class="pic" src="../assets/Turpan_04.jpg" alt="" />
 
-            <img @load="imgLoad" class="pic" src="../assets/Turpan_05.jpg" alt="" />
+      <img @load="imgLoad" class="pic" src="../assets/Turpan_05.jpg" alt="" />
 
-            <img @load="imgLoad" class="pic" src="../assets/Turpan_06.jpg" alt="" />
+      <img @load="imgLoad" class="pic" src="../assets/Turpan_06.jpg" alt="" />
 
-            <img @load="imgLoad" class="pic" src="../assets/Turpan_07.jpg" alt="" />
-        </div>
-    </section>
+      <img @load="imgLoad" class="pic" src="../assets/Turpan_07.jpg" alt="" />
+    </div>
+  </section>
 
-    <section class="hp-loading" v-show="false">
-        <!-- <p class="loading-text">Ekar</p> -->
-        <div class="loading">
-            <div></div>
-        </div>
-    </section>
+  <section class="hp-loading" v-show="false">
+    <!-- <p class="loading-text">Ekar</p> -->
+    <div class="loading">
+      <div></div>
+    </div>
+  </section>
 </template>
 
-<style>
-
-</style>
+<style></style>
