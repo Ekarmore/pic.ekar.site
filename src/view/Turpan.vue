5638670154e786fa06d499c4250aad9368a42245
EKar <Earthb0y@outlook.com>
2022-10-01 13:22:27 +0800

    mouse event

--- src/view/Turpan.vue
+++ src/view/Turpan.vue
@@ -22,7 +22,6 @@ const [imgLoad, imgLoad2, imgLoad3, imgLoad4] = [
 
 // 获取 colBox
 const colBox = ref(null);
-
 //wow init
 // new WOW({
 //   boxClass: "wow",
@@ -38,57 +37,72 @@ const colBox = ref(null);
 //   resetAnimation: true, // reset animation on end (default is true)
 // }).init();
 
+
 // 滚轮事件
 const wheel = (event) => {
+
   //阻止默认事件触发
-  //   event.preventDefault();
-  // 滚动
-  console.log(event.deltaX);
-  colBox.value.scrollLeft += (event.deltaY)*1.5;
+  // event.preventDefault();
+  //滚动
+  let xGap = ref(event.deltaY)
+  let addX = setInterval(() => {
+    colBox.value.scrollLeft += (xGap.value)/2;
+  }, 10);
+
+  setTimeout(() => {
+    window.clearInterval(addX)
+  }, 50);
+
+  // colBox.value.scrollLeft += (yGap.value)/2;
+  // colBox.value.scrollLeft += (yGap.value)/4;
+  // colBox.value.scrollLeft += (yGap.value)/8;
 };
+
 </script>
 <template>
   <section v-show="a&&a1&&a2&&a3">
-  <section class="navBar">
-    <Nav class="nav-pc" />
+    <section class="navBar">
+      <Nav class="nav-pc" />
+    </section>
+    <section class="picture_container">
+      <div ref="colBox" class="col-box" @wheel="wheel">
+        <div id="pic-about-text" class="pic">向右滑动或滚动鼠标滚轮 -></div>
+
+        <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
+
+        <img @load="imgLoad1" class="pic" src="../assets/Turpan_02.jpg" alt="" />
+
+        <img @load="imgLoad2" class="pic" src="../assets/Turpan_01.jpg" alt="" />
+
+        <img @load="imgLoad3" class="pic" src="../assets/Turpan_03.jpg" alt="" />
+
+        <img @load="imgLoad4" class="pic" src="../assets/Turpan_04.jpg" alt="" />
+
+        <img @load="imgLoad" class="pic" src="../assets/Turpan_05.jpg" alt="" />
+
+        <img @load="imgLoad" class="pic" src="../assets/Turpan_06.jpg" alt="" />
+
+        <img @load="imgLoad" class="pic" src="../assets/Turpan_07.jpg" alt="" />
+
+        <div id="pic-about-text" class="pic">向右滑动或滚动鼠标滚轮 -></div>
+      </div>
+    </section>
+
+    <section class="hp-loading" v-show="false">
+      <!-- <p class="loading-text">Ekar</p> -->
+      <div class="loading">
+        <div></div>
+      </div>
+    </section>
   </section>
-  <section class="picture_container">
-    <div ref="colBox" class="col-box" @wheel="wheel">
-      <div id="pic-about-text" class="pic">向右滑动或滚动鼠标滚轮  -></div>
 
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_020.jpg" alt="" />
-
-      <img @load="imgLoad1" class="pic" src="../assets/Turpan_02.jpg" alt="" />
-
-      <img @load="imgLoad2" class="pic" src="../assets/Turpan_01.jpg" alt="" />
-
-      <img @load="imgLoad3" class="pic" src="../assets/Turpan_03.jpg" alt="" />
-
-      <img @load="imgLoad4" class="pic" src="../assets/Turpan_04.jpg" alt="" />
-
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_05.jpg" alt="" />
-
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_06.jpg" alt="" />
-
-      <img @load="imgLoad" class="pic" src="../assets/Turpan_07.jpg" alt="" />
-
-      <div id="pic-about-text" class="pic">向右滑动或滚动鼠标滚轮 -></div>
-    </div>
-  </section>
-
-  <section class="hp-loading" v-show="false">
-    <!-- <p class="loading-text">Ekar</p> -->
+  <section class="hp-loading" v-show="!(a&&a1&&a2&&a3)">
     <div class="loading">
       <div></div>
     </div>
   </section>
-  </section>
-
-<section class="hp-loading" v-show="!(a&&a1&&a2&&a3)">
-        <div class="loading">
-            <div></div>
-        </div>
-    </section>
 </template>
 
-<style></style>
+<style>
+
+</style>
