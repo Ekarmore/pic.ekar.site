b43794dc03d77bee6519273cb2ef0f7d94abab1f
EKar <Earthb0y@outlook.com>
2022-10-11 18:29:14 +0800

    work element

--- src/view/Unnoticed.vue
+++ src/view/Unnoticed.vue
@@ -11,7 +11,7 @@ useModal(a,a1,a2,a3)
     <section v-show="a && a1 && a2 && a3">
       <section class="picture_container">
         <div ref="colBox" class="col-box" @wheel="wheel">
-          <article class="un-box"><p class="un-text"><p class="un-title">Unnoticed</p>说说我最近常去的江边,起初大概是因为离我的居所很近加之所在的大学城岛也并不大，如果想要出门走走会很快就走到江边。但以前大多都会骑车路过，所以会没有留意到许多。很多次路过环岛的几处江边觉得可能会有自已想要走近看看的风景可也没有真正的停下来去看，之后因为空闲时间变多和去江边实在是容易，会常常在江边徒步。在江边行走几次之后我会感到有些奇怪，江水将岛分割了起来，这让江边成为了岛上不只是地理上的最边缘，被扔在草坪上生产日期是前几个月的食品包装袋或是被植缠满的生锈单车都在说明着这里鲜有被打理的时候。好在它并不就此死寂，当走在江边会看到许多景象，它们由充满生机的部分构成。</p></article>
+          <article class="un-box"><p class="un-text"><p class="un-title">Unnoticed</p>说说我最近常去的江边,起初大概是因为离我的居所很近加之所在的大学城岛也并不大，如果想要出门走走会很快就走到江边。但以前大多都会骑车路过，所以会没有留意到许多。很多次路过环岛的几处江边觉得可能会有自已想要走近看看的风景可也没有真正的停下来去看，之后因为空闲时间变多和去江边实在是容易，会常常在江边徒步。在江边行走几次之后我会感到有些奇怪，江水将岛分割了起来，这让江边成为了岛上不只是地理上的最边缘，被扔在草坪上生产日期是前几个月的食品包装袋或是被植缠满的生锈单车都在说明着这里鲜有被打理的时候。好在它并不就此死寂，当走在江边会看到许多景象，它们由充满生机的部分构成。<p class="text-scroll">向右滑动 ></p> </p></article>
           <!-- <img class="pic" src="../assets/Un_text.jpg" alt="" /> -->
           <!-- <div class="pic"> <article class="un-text" ><p class="un-tt">说说我最近常去的江边，起初大概是因为离我的居所很近加之所在的大学城岛也并不大，如果想要出门走走会很快就走到江边。但以前大多都会骑车路过，所以会没有留意到许多。很多次路过环岛的几处江边觉得可能会有自已想要走近看看的风景可也没有真正的停下来去看，之后因为空闲时间变多和去江边实在是容易，会常常在江边徒步。在江边行走几次之后我会感到有些奇怪，江水将岛分割了起来，这让江边成为了岛上不只是地理上的最边缘，被扔在草坪上生产日期是前几个月的食品包装袋或是被植缠满的生锈单车都在说明着这里鲜有被打理的时候。好在它并不就此死寂，当走在江边会看到许多景象，它们由充满生机的部分构成。</p></article> </div> -->
           <img @load="imgLoad" class="pic" src="../assets/Un_1.webp" alt="" />
@@ -39,12 +39,14 @@ useModal(a,a1,a2,a3)
 </template>
 
 <style>
-
+.text-scroll{
+@apply text-center text-sm p-20 font-mono font-bold
+}
 .un-box{
   @apply flex flex-col justify-center  min-w-fit md:mr-5; 
 }
 .un-text{
-@apply text-xs md:text-sm leading-8 pl-10 pr-10 md:pl-16 md:pr-16 lg:pl-32 lg:pr-32 xl:pl-48 xl:pr-48
+@apply text-xs leading-8 md:leading-8 md:text-sm pl-10 pr-10 md:pl-16 md:pr-16 lg:pl-32 lg:pr-32 xl:pl-48 xl:pr-48
 }
 /* .un-text{
 @apply text-sm leading-8 pt-5 pl-10 pr-10 md:pl-16 md:pr-16 lg:pl-32 lg:pr-32 xl:pl-48 xl:pr-48
