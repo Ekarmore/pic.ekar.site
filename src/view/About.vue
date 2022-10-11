50f03144da4db20ed83ffdd7d55a4912a27832ec
EKar <Earthb0y@outlook.com>
2022-10-11 20:58:22 +0800

    work element

--- src/view/About.vue
+++ src/view/About.vue
@@ -14,7 +14,7 @@ watchEffect(() => {
       <p class="me-text-start">Hello,yo!</p>
 
       <p class="about-me">我是<a class="Ekar" href="">YEKMAT</a>,来自这颗蓝色星球某一处的氧气呼吸者,前端开发者,自由摄影师。<br /></p>
-      <p class="about-me"> 我创建了这个网站并在这里放了一些自己拍的照片供于访问与分享,你可以在这里看到我在做的一些摄影项目,希望当中会有能够与你产生共鸣的照片。<br />
+      <p class="about-me"> 我创建了这个网站并在这里放了一些自己拍摄的照片用于归纳与分享,你可以在这里看到我在做的一些摄影项目,希望当中会有能够与你产生共鸣的照片。<br />
         多说无益,在这里或许你更应该从照片里了解我。</p>
 
       <p class="startBox">所以,<router-link class="getStart" to="/">点击我开始观看作品 ></router-link>
--- src/view/Unnoticed.vue
+++ src/view/Unnoticed.vue
@@ -43,10 +43,10 @@ useModal(a,a1,a2,a3)
 @apply text-center text-sm p-10 font-mono font-normal 
 }
 .un-box{
-  @apply flex flex-col justify-center min-w-fit md:mr-5; 
+  @apply flex flex-col justify-center min-w-fit md:mr-5 2xl:pl-48 2xl:pr-48; 
 }
 .un-text{
-@apply text-xs leading-8 md:leading-8 md:text-sm pl-10 pr-10 md:pl-16 md:pr-16 lg:pl-32 lg:pr-32 xl:pl-48 xl:pr-48
+@apply text-xs leading-8 md:leading-8 md:text-sm pl-10 pr-10 md:pl-16 md:pr-16 lg:pl-32 lg:pr-32 xl:pl-48 xl:pr-48 2xl:pr-96 2xl:pl-96
 }
 /* .un-text{
 @apply text-sm leading-8 pt-5 pl-10 pr-10 md:pl-16 md:pr-16 lg:pl-32 lg:pr-32 xl:pl-48 xl:pr-48
