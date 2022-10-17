f1fbf2ad2d369fabb68fcd98804163bcc24ee207
EKar <Earthb0y@outlook.com>
2022-10-17 19:52:16 +0800

    workpage change

--- src/view/Turpan.vue
+++ src/view/Turpan.vue
@@ -1,24 +1,25 @@
 <script setup>
-import pic1 from '../assets/T_1.webp'
-import pic2 from '../assets/T_2.webp'
-import pic3 from '../assets/T_19.webp'
-import pic4 from '../assets/T_24.webp'
-import {useImgLoadNew} from '../utils/LoadingNew'
+// import pic1 from '../assets/T_1.webp'
+// import pic2 from '../assets/T_2.webp'
+// import pic3 from '../assets/T_19.webp'
+// import pic4 from '../assets/T_24.webp'
+// import {useImgLoadNew} from '../utils/LoadingNew'
 import { ref, watchEffect, watch, onMounted, reactive } from 'vue'
 import { useImgLoad } from '../utils/Loading'
 import { useXScroll } from '../utils/scrollControl'
 import { useModal } from '../utils/modalControl'
-// import { useIntersectionObserver } from '@vueuse/core'
+import { useIntersectionObserver } from '@vueuse/core'
 import { useLazy} from '../utils/lazyLoad'
-const {imgLoad,a} = useImgLoadNew()
-watch(a,()=>{
-  console.log(a.value);
-})
+
+
 // loading
-// const { imgLoad, imgLoad2, imgLoad3, imgLoad4, a, a1, a2, a3 } = useImgLoad()
+const { imgLoad, imgLoad2, imgLoad3, imgLoad4, a, a1, a2, a3 } = useImgLoad()
+useModal(a, a1, a2, a3)
 // col wheel
-// const { colBox, wheel } = useXScroll()
+const { colBox, wheel } = useXScroll()
+
 // lazy load
+
 // const {lazyLoad} = useLazy(target1)
 // watch(lazyLoad,()=>{
 //   lazyLoad1.value = lazyLoad.value
@@ -27,50 +28,51 @@ watch(a,()=>{
 // const lazyLoad1 = ref(false)
 // const picNew = ref('picNew')
 // const target1 = ref(null)
+
 // pic animate
-const pic = ref('pic')
-const openPic = ref('openPic')
-const closePic = ref('closePic')
+
+// const pic = ref('pic')
+// const openPic = ref('openPic')
+// const closePic = ref('closePic')
 // control modal
-// useModal(a, a1, a2, a3)
 
-const imgs = reactive([
-  {name:'T_1',src:pic1},
-  {name:'T_2',src:pic2},
-  {name:'T_3',src:pic3},
-  {name:'T_4',src:pic4},
-])
+// const imgs = reactive([
+//   {name:'T_1',src:pic1},
+//   {name:'T_2',src:pic2},
+//   {name:'T_3',src:pic3},
+//   {name:'T_4',src:pic4},
+// ])
 
-const imgSrc = ref('')
+// const imgSrc = ref('')
 
-imgSrc.value = pic1
+// imgSrc.value = pic1
 
-const i = ref(0)
+// const i = ref(0)
 
-const next = ()=>{
+// const next = ()=>{
 
-if(i.value<imgs.length-1){
-  i.value += 1
-}else{
-  i.value = 0
-}
-imgSrc.value = imgs[i.value].src
-}
-const prev = () =>{
+// if(i.value<imgs.length-1){
+//   i.value += 1
+// }else{
+//   i.value = 0
+// }
+// imgSrc.value = imgs[i.value].src
+// }
+// const prev = () =>{
 
-if(i.value>=1){
-  i.value -= 1
-}
-else{
- i.value = 0
-}
-imgSrc.value = imgs[i.value].src
-}
+// if(i.value>=1){
+//   i.value -= 1
+// }
+// else{
+//  i.value = 0
+// }
+// imgSrc.value = imgs[i.value].src
+// }
 
 </script>
 <template>
   <div>
-    <!-- <section v-show="a&&a1&&a2&&a3">
+    <section v-show="a&&a1&&a2&&a3">
       <section class="picture_container">
         <div ref="colBox" class="col-box" @wheel="wheel">
           <img @load="imgLoad" class="pic" src="../assets/T_1.webp" alt="" />
@@ -82,7 +84,7 @@ imgSrc.value = imgs[i.value].src
           <img class="pic" src="../assets/T_272.webp" alt="" />
           <img class="pic" src="../assets/T_292.webp" alt="" />
           <img class="pic" src="../assets/T_0000.webp" alt="" />
-          <img ref="target1" :class='[lazyLoad1? openPic:closePic,pic]' src="../assets/T_23.webp" alt="" />
+          <!-- <img ref="target1" :class='[lazyLoad1? openPic:closePic,pic]' src="../assets/T_23.webp" alt="" /> -->
           <img class="pic" src="../assets/T_5.webp" alt="" />
           <img class="pic" src="../assets/T_28.webp" alt="" />
           <img class="pic" src="../assets/T_282.webp" alt="" />
@@ -95,20 +97,21 @@ imgSrc.value = imgs[i.value].src
           <img class="pic-end" src="../assets/T_6.webp" alt="" />
         </div>
       </section>
-    </section> -->
+    </section>
+
     <!-- new pic -->
-    <section class="picture_container_new" >
+    <!-- <section class="picture_container_new" >
       <div ref="colBox" class="col-box-new">
         <img @load="imgLoad" :key="1111" class="picNew" :src='imgSrc' alt="">
       </div>
       <div class="img-control"><div class="prev" @click="prev">prev</div> / <div class="next" @click="next">next</div></div>
-    </section>
+    </section> -->
 
-    <!-- <section class="hp-loading" v-show="!(a&&a1&&a2&&a3)">
+    <section class="hp-loading" v-show="!(a&&a1&&a2&&a3)">
       <div class="loading">
         <div></div>
       </div>
-    </section> -->
+    </section>
   </div>
 </template>
 
