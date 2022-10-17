61978bae3726ff3aae1e6dc6f9ac7d6384d23689
EKar <Earthb0y@outlook.com>
2022-10-17 21:00:23 +0800

    workpage change

--- src/view/Turpan.vue
+++ src/view/Turpan.vue
@@ -37,32 +37,6 @@ const { colBox, wheel } = useXScroll()
           <img class="pic" src="../assets/T_25.webp" alt="" />
           <img class="pic-end" src="../assets/T_6.webp" alt="" />
         </div>
-        <!-- <div ref="colBox" class="col-box" @wheel="wheel">
-          <img @load="imgLoad" class="pic" src="../assets/T_1.webp" alt="" />
-          <img @load="imgLoad2" class="pic" src="../assets/T_2.webp" alt="" />
-          <img @load="imgLoad3" class="pic" src="../assets/T_19.webp" alt="" />
-          <img @load="imgLoad4" class="pic" src="../assets/T_24.webp" alt="" />
-          <img class="pic" src="../assets/T_4.webp" alt="" />
-          <img class="pic" src="../assets/T_3.webp" alt="" />
-          <img class="pic" src="../assets/T_0000.webp" alt="" />
-          <img class="pic" src="../assets/T_5.webp" alt="" />
-          <img class="pic" src="../assets/T_28.webp" alt="" />
-          <img @load="imgLoad" class="pic" src="../assets/T_1.webp" alt="" />
-          <img class="pic" src="../assets/T_0000.webp" alt="" />
-          <img class="pic" src="../assets/T_5.webp" alt="" />
-          <img class="pic" src="../assets/T_00000.webp" alt="" />
-          <img class="pic" src="../assets/T_28.webp" alt="" />
-          <img class="pic" src="../assets/T_0000.webp" alt="" />
-          <img class="pic" src="../assets/T_5.webp" alt="" />
-          <img @load="imgLoad" class="pic" src="../assets/T_1.webp" alt="" />
-          <img class="pic" src="../assets/T_28.webp" alt="" />
-          <img class="pic" src="../assets/T_0000.webp" alt="" />
-          <img class="pic" src="../assets/T_5.webp" alt="" />
-          <img class="pic" src="../assets/T_00000.webp" alt="" />
-          <img class="pic" src="../assets/T_00000.webp" alt="" />
-          <img class="pic" src="../assets/T_28.webp" alt="" />
-
-        </div> -->
       </section>
     </section>
     <section class="hp-loading" v-show="!(a&&a1&&a2&&a3)">
