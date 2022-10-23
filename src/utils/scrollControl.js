a8d57905ab8289526b6f7f612e205fe260bb137f
EKar <Earthb0y@outlook.com>
2022-10-23 17:31:14 +0800

    workpage change

--- src/utils/scrollControl.js
+++ src/utils/scrollControl.js
@@ -4,17 +4,24 @@ export function useXScroll() {
   // 获取 colBox
   const colBox = ref(null);
   // 滚轮事件
+  // function wheel(event) {
+  //   //阻止默认事件触发
+  //   // event.preventDefault();
+  //   //滚动
+  //   let disX = ref(event.deltaY);
+  //   let scrollX = setInterval(() => {
+  //     colBox.value.scrollLeft += disX.value;
+  //   }, 10);
+  //   setTimeout(() => {
+  //     window.clearInterval(scrollX);
+  //   }, 50);
+  // }
   function wheel(event) {
     //阻止默认事件触发
     // event.preventDefault();
     //滚动
     let disX = ref(event.deltaY);
-    let scrollX = setInterval(() => {
-      colBox.value.scrollLeft += disX.value;
-    }, 10);
-    setTimeout(() => {
-      window.clearInterval(scrollX);
-    }, 50);
+    colBox.value.scrollLeft += disX.value*2;
   }
   return { colBox, wheel };
 }
