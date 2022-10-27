7385e5ec02f7c045dc65008ff70f629bbaf61f66
EKar <Earthb0y@outlook.com>
2022-10-27 13:24:51 +0800

    add work

--- src/view/Another_Landscape.vue
+++ src/view/Another_Landscape.vue
@@ -17,13 +17,13 @@ useModal(a,a1,a2,a3)
         <section v-show="a&&a1">
             <section :style="{height:newVh}" class="picture_container">
                 <div ref="colBox" class="col-box" @wheel="wheel">
-                    <img @load="imgLoad" class="pic" src="../assets/AL_2.webp" alt="" />
-                    <img @load="imgLoad2" class="pic" src="../assets/AL_9.webp" alt="" />
-                    <img @load="imgLoad3" class="pic" src="../assets/AL_11.webp" alt="" />
-                    <img @load="imgLoad4" class="pic" src="../assets/AL_1.webp" alt="" />
-                    <img  class="pic" src="../assets/AL_3.webp" alt="" />
-                    <img  class="pic" src="../assets/AL_4.webp" alt="" />
-                    <img class="pic-end" src="../assets/AL_5.webp" alt="" />
+                    <img @load="imgLoad" class="pic" src="https://s1.vika.cn/space/2022/10/27/511d72c4df3b4ab4aacc169b05478df1?attname=AL_7.webp" alt="" />
+                    <img @load="imgLoad2" class="pic" src="https://s1.vika.cn/space/2022/10/27/4c210d6907ae4ec9a340174e61901113?attname=AL_9.webp" alt="" />
+                    <img @load="imgLoad3" class="pic" src="https://s1.vika.cn/space/2022/10/27/03595704918442d3823478786ce990cf?attname=AL_11.webp" alt="" />
+                    <img @load="imgLoad4" class="pic" src="https://s1.vika.cn/space/2022/10/27/d802f7104cd24aa3bf9f91586e93d9d4?attname=AL_1.webp" alt="" />
+                    <img  class="pic" src="https://s1.vika.cn/space/2022/10/27/9d7936985b5242ba9d84b531f0b73bc2?attname=AL_3.webp" alt="" />
+                    <img  class="pic" src="https://s1.vika.cn/space/2022/10/27/c9261febfa55467794e2351957777efe?attname=AL_4.webp" alt="" />
+                    <img class="pic-end" src="https://s1.vika.cn/space/2022/10/27/7f7ecdd8008f48a4bcdb16b4cbda75b8?attname=AL_5.webp" alt="" />
                 </div>
             </section>
         </section>
