1e5dbd3784439701ef5c40c579edcc7b41e83745
EKar <Earthb0y@outlook.com>
2022-11-11 18:04:04 +0800

    statistical

--- src/main.js
+++ src/main.js
@@ -8,15 +8,3 @@ const app = createApp(App)
 app.use(router)
 app.mount('#app')
 
-const MODE = import.meta.env.MODE
-if (MODE !== 'development') {
-  // eslint-disable-next-line no-var, vars-on-top
-  var _hmt = _hmt || [];
-  (function () {
-    const hm = document.createElement('script')
-    hm.src = 'https://hm.baidu.com/hm.js?bab3d289d9e246576d5731056315a9b3'
-    const s = document.getElementsByTagName('script')[0]
-    s.parentNode.insertBefore(hm, s)
-  })()
-}
-
