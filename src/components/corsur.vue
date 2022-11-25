997babb602bb8f9429fcb7744b62ee93c07af0b2
EKar <Earthb0y@outlook.com>
2022-11-25 22:28:51 +0800

    build page

--- src/components/corsur.vue
+++ src/components/corsur.vue
@@ -1,24 +1,24 @@
 <script setup>
 import { useMouse } from '@vueuse/core'
 import { onMounted, ref, watchEffect } from 'vue'
-const mouse = ref(null)
+const cursor = ref(null)
 const { x, y } = useMouse()
 onMounted(() => {
   watchEffect(() => {
-    mouse.value.style.top = `${y.value - 8}px`
-    mouse.value.style.left = `${x.value - 8}px`
+    cursor.value.style.top = `${y.value - 8}px`
+    cursor.value.style.left = `${x.value - 8}px`
   })
 })
 </script>
 
 <template>
   <Teleport to=".nav-pc">
-    <div ref="mouse" class="mouse" @click="changeMouse" />
+    <div ref="cursor" class="cursor" @click="changeMouse" />
   </Teleport>
 </template>
 
 <style>
-.mouse{
+.cursor{
 @apply hidden md:flex justify-center items-center transition-transform duration-200 ease-in-out rounded-full absolute  w-3 h-3  pointer-events-none backdrop-opacity-100 backdrop-invert
 }
 </style>
