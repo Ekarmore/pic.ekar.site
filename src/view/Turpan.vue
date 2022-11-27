51cf9930729dc5f08e463dafec4bac54a86c7f73
EKar <Earthb0y@outlook.com>
2022-11-27 21:44:20 +0800

    build page

--- src/view/Turpan.vue
+++ src/view/Turpan.vue
@@ -112,9 +112,9 @@ const imgLoad = () => {
       </transition>
     </div>
     <div class="text-center absolute bottom-7 items-center text-xs">
-      <span hover:bg-black hover:text-white pl-2 pr-2 font-serif @click="Prev">Prev</span>
+      <span md:hover:bg-black md:hover:text-white pl-2 pr-2 font-serif @click="Prev">Prev</span>
       <span text-xs pl-1 pr-1>/</span>
-      <span hover:bg-black hover:text-white pl-2 pr-2 font-serif @click="Next">Next</span>
+      <span md:hover:bg-black md:hover:text-white pl-2 pr-2 font-serif @click="Next">Next</span>
       <span font-serif font-xs pl-2>({{ List[item].id }} of {{ List.length }})</span>
     </div>
   </section>
