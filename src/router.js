315b85af5aeef9980b21092111f501c837e0a03e
EKar <Earthb0y@outlook.com>
2022-12-06 12:11:08 +0800

    page build

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import AnotherLandscape from './view/Another_Landscape.vue'
 import Turpan from './view/Turpan.vue'
 import NeverKnowHowMuchILoveYou from './view/NeverMuchKnow.vue'
 import About from './view/About.vue'
+import grid from './view/grid.vue'
 // import Layout from './view/Layout.vue'
 
 const router = createRouter({
@@ -15,10 +16,12 @@ const router = createRouter({
   routes: [
     { path: '/', component: NeverKnowHowMuchILoveYou, meta: { title: 'NeverKnowHowMuchILoveYou | ekar' } },
     { path: '/Unnoticed', component: Unnoticed, meta: { title: 'Unnoticed | ekar' } },
+    { path: '/', component: NeverKnowHowMuchILoveYou, meta: { title: 'NeverKnowHowMuchILoveYou | ekar' } },
     { path: '/AnotherLandscape', component: AnotherLandscape, meta: { title: 'AnotherLandscape | ekar' } },
     { path: '/Turpan', component: Turpan, meta: { title: 'Turpan | Ekar' } },
     { path: '/NeverKnowHowMuchILoveYou', component: NeverKnowHowMuchILoveYou, meta: { title: 'Never Know how much i love you  | ekar' } },
     { path: '/about', component: About, meta: { title: 'About | ekar' } },
+    { path: '/grid', component: grid, meta: { title: 'About | ekar' } },
     // { path: '/layout', component: Layout, meta: { title: 'About | ekar' } },
   ],
 })
---
+++ src/view/grid.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="h-screen bg-dark-200 grid grid-cols-10 grid-rows-10">
+    <div class="bg-blue-700 row-span-5 gap-2 m-1 ">
+      1
+    </div>
+    <div class="bg-blue-700 col-span-10 row-span-4 gap-2 m-1 ">
+      2
+    </div>
+    <div class="bg-blue-700  gap-2 m-1">
+      3
+    </div>
+    <div class="bg-blue-700 gap-2 m-1 ">
+      4
+    </div>
+    <div class="bg-blue-700 gap-2 m-1 ">
+      5
+    </div>
+    <div class="bg-blue-700 gap-2 m-1">
+      6
+    </div>
+    <div class="bg-blue-700 gap-2 m-1 ">
+      7
+    </div>
+    <div class="bg-blue-700 gap-2 m-1">
+      8
+    </div>
+    <div class="bg-blue-700 gap-2 m-1">
+      9
+    </div>
+    <div class="bg-blue-700 gap-2 m-1">
+      10
+    </div>
+  </div>
+</template>
