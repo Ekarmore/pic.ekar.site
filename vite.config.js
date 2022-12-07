cfa64bfd559f4ebedddbf4ee255db4a85d6d770d
EKar <Earthb0y@outlook.com>
2022-12-07 23:03:47 +0800

    page build

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,15 @@
 import { defineConfig } from 'vite'
 import Unocss from 'unocss/vite'
-import presetUno from '@unocss/preset-uno'
-import presetWebFonts from '@unocss/preset-web-fonts'
-import presetAttributify from '@unocss/preset-attributify'
+// import presetUno from '@unocss/preset-uno'
+// import presetWebFonts from '@unocss/preset-web-fonts'
+// import presetAttributify from '@unocss/preset-attributify'
 import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Unocss({
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetWebFonts({
-          fonts: {
-            sans: 'DM Sans',
-            serif: ['Vollkorn', 'Noto Serif Traditional Chinese'],
-            mono: 'DM Mono',
-          },
-        }),
-      ],
-    })],
+    Unocss(),
+  ],
   server: {
     host: '0.0.0.0',
   },
